ac635a06eed80ab308cd87bc7ed96d8d196e47fa
majusiregar@gmail.com <47182263+bangcadel@users.noreply.github.com>
2021-12-12 11:53:03 +0700

    add-service-mahasiswa

---
+++ lib/envirotment/file_env.dart
@@ -0,0 +1,3 @@
+class FileEnv {
+  static const endpointMahasiswaRead = 'http://10.0.2.2:5000/mahasiswa/read';
+}
---
+++ lib/services/mahasiswa_service.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:myclient/envirotment/file_env.dart';
+import 'package:myclient/models/mahasiswa/mahasiswa_model.dart';
+
+class MahasiswaService {
+
+  static Future<Mahasiswa> readMahasiswa() async {
+    final response = await http.get(Uri.parse(FileEnv.endpointMahasiswaRead));
+
+    if(response.statusCode == 200){
+      return Mahasiswa.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load mahasiswa');
+    }
+  }
+}
