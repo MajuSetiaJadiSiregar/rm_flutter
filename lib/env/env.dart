644d84af75ce615d63e52cfe8583f6a04110b9b2
majusiregar@gmail.com <47182263+bangcadel@users.noreply.github.com>
2021-12-14 11:18:24 +0700

    add-endpoint-delete

--- lib/env/env.dart
+++ lib/env/env.dart
@@ -1,3 +1,4 @@
 class Envirotment {
   static const endpointReadMahasiswa = "http://10.0.2.2:5000/mahasiswa/read";
+  static const endpointDeleteMahasiswa = "http://10.0.2.2:5000/mahasiswa/delete/";
 }
